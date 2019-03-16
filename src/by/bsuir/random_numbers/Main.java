643f594818afe6df1ae7a36b8458e10df66f1caa
AndrewKostuk <andrkost78@gmail.com>
2019-03-16 14:28:57 +0300

    add constants

--- src/by/bsuir/random_numbers/Main.java
+++ src/by/bsuir/random_numbers/Main.java
@@ -3,21 +3,22 @@ package by.bsuir.random_numbers;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class RundomNumber extends Application {
+public class Main extends Application {
+	private static final String TITLE = "toGuessRandom";
 
 	private PaneCreator newPane;
-	
-	public RundomNumber() {
-		newPane=new PaneCreator();
+
+	public Main() {
+		newPane = new PaneCreator();
 	}
-	
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("toGuessRandom");
+		primaryStage.setTitle(TITLE);
 		primaryStage.setResizable(true);
 
 		newPane.createDefault();
--- src/by/bsuir/random_numbers/PaneCreator.java
+++ src/by/bsuir/random_numbers/PaneCreator.java
@@ -13,79 +13,83 @@ import javafx.scene.layout.GridPane;
 import java.util.Random;
 
 public class PaneCreator {
+	private static final String BUTTON_LABEL_1 = "Угадать";
+	private static final String BUTTON_LABEL_2 = "Новое";
+	private static final String HELP_LABEL_1 = "Ваше число меньше загаданного";
+	private static final String HELP_LABEL_2 = "Ваше число больше загаданного";
+	private static final String HELP_LABEL_3 = "Вы абсолютно правы";
+	private static final int BOUND_FOR_RANDOM_NUMBER = 101;
+
 	private Random random;
 	private int randomNumber;
 
 	private GridPane gridpane;
 	private BorderPane root;
 	private Scene scene;
-	
+
 	private Button guessButton;
 	private Button newRandomButton;
 	private Label helpLabel;
 	private TextField customerChooseTextField;
 
-	public PaneCreator() {	
+	public PaneCreator() {
 		random = new Random();
-		randomNumber = random.nextInt(101);
+		randomNumber = random.nextInt(BOUND_FOR_RANDOM_NUMBER);
 
 		gridpane = new GridPane();
 		root = new BorderPane();
 		scene = new Scene(root, 330, 150);
-		
-		guessButton = new Button("Угадать");
-		newRandomButton = new Button("Новое");
-		helpLabel = new Label("");		
+
+		guessButton = new Button(BUTTON_LABEL_1);
+		newRandomButton = new Button(BUTTON_LABEL_2);
+		helpLabel = new Label("");
 		customerChooseTextField = new TextField();
 	}
-	
-	
+
 	public Scene getScene() {
 		return scene;
 	}
 
 	public void createDefault() {
-			gridpane.setPadding(new Insets(10));//расстояние от краев окна до кнопок и текст.полей = 10
-			gridpane.setHgap(100);						
-			gridpane.setVgap(10);
-			
-			for (int i = 0; i < 1; i++) {						//создать 2 колонки 
-			ColumnConstraints column = new ColumnConstraints(); 
-			column.setPercentWidth(100 / 2.0);					//чтобы занимала каждая 50% окна
-			gridpane.getColumnConstraints().add(column);		//и добавить на нашу сетку окна
-			}
+		gridpane.setPadding(new Insets(10)); // расстояние от краев окна до кнопок и текст.полей = 10
+		gridpane.setHgap(100);
+		gridpane.setVgap(10);
+
+		for (int i = 0; i < 1; i++) { // создать 2 колонки
+			ColumnConstraints column = new ColumnConstraints();
+			column.setPercentWidth(100 / 2.0); // чтобы занимала каждая 50% окна
+			gridpane.getColumnConstraints().add(column); // и добавить на нашу сетку окна
+		}
 	}
-	
+
 	public void addComponents() {
-		gridpane.add(customerChooseTextField, 0, 0);		
-		gridpane.add(helpLabel, 0, 1, 2, 1);	
-		gridpane.add(guessButton, 0, 2);	
+		gridpane.add(customerChooseTextField, 0, 0);
+		gridpane.add(helpLabel, 0, 1, 2, 1);
+		gridpane.add(guessButton, 0, 2);
 		gridpane.add(newRandomButton, 1, 2);
 		root.setCenter(gridpane);
 	}
-	
-	
+
 	public void setActionsToButtons() {
 		newRandomButton.setOnAction(new EventHandler<ActionEvent>() { // как должна реагировать
-			public void handle(ActionEvent e) { 					  // кнопка на нажатие
-				randomNumber = random.nextInt(101); 
-			} 
-		}); 
-		
+			public void handle(ActionEvent e) { // кнопка на нажатие
+				randomNumber = random.nextInt(BOUND_FOR_RANDOM_NUMBER);
+			}
+		});
+
 		guessButton.setOnAction(new EventHandler<ActionEvent>() { // как должна реагировать другая кнопка на нажатие
 			public void handle(ActionEvent e) {
 				Integer customerChoose = Integer.parseInt(customerChooseTextField.getText());
 				if (customerChoose < randomNumber) {
-					helpLabel.setText("Ваше число меньше загаданного");
-				} 
-				else {
+					helpLabel.setText(HELP_LABEL_1);
+				} else {
 					if (customerChoose > randomNumber) {
-						helpLabel.setText("Ваше число больше загаданного");
+						helpLabel.setText(HELP_LABEL_2);
 					} else
-						helpLabel.setText("Вы абсолютно правы");
+						helpLabel.setText(HELP_LABEL_3);
 				}
 			}
-		});	
+		});
 	}
-		
+
 }
